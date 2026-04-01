d4d116e35c968c6b453daacfb99ac602f8cc1ea9
Сковлюк Матвій Сергійович <ipz224_sms@student.ztu.edu.ua>
2026-04-02 02:58:27 +0300

    Додав метод getUserid у моделі користувача

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,10 @@
 const db = require('../config/db');
+module.exports = {
+    getDefaultRoleId,
+    getUserid,
+    createUser,
+    findUserByEmailWithRole
+};
 
 const getDefaultRoleId = async () => {
     const roleNames = ['user', 'client', 'customer'];
@@ -71,9 +77,20 @@ const findUserByEmailWithRole = async (email) => {
     return rows[0] || null;
 };
 
-module.exports = {
-    getDefaultRoleId,
-    createUser,
-    findUserByEmailWithRole
+
+const getUserid = async (id) => {
+    const [rows] =  await db.execute(
+        `SELECT 
+         u.id, u.role_id, r.role_name,
+        u.first_name, u.last_name, u.email,
+u.phone, u.avatar_url, u.loyalty_points
+        FROM users u
+        INNER JOIN roles r ON u.role_id = r.id
+        WHERE u.id = ?
+        LIMIT 1`,
+        [id]
+    );
+
+    return rows[0] || null;
 };
 
