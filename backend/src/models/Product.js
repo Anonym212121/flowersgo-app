fce9e665d62eaa02b5df83088d3f86d26722c27d
Сковлюк Матвій Сергійович <ipz224_sms@student.ztu.edu.ua>
2026-04-02 19:32:30 +0300

    added search form

--- backend/src/models/Product.js
+++ backend/src/models/Product.js
@@ -1,37 +1,37 @@
-const db = require('../config/db');
-const allProducts = async (categoryId = null) => {
-    if (!categoryId) {
-    const [rows] = await db.execute(
-            `SELECT
-                p.id, p.name, p.slug, p.description, p.base_price, p.sale_price,
-                p.stock_quantity, p.image_url, p.average_rating,
-                c.name AS category_name
-              FROM products p
-             INNER JOIN categories c ON p.category_id = c.id
-             WHERE p.is_active = 1
-             ORDER  BY p.id DESC`
-        );
-          return rows;
-    }
-    const [rows] = await db.execute(
-        `SELECT
-            p.id, p.name, p.slug, p.description, p.base_price, p.sale_price,
-            p.stock_quantity, p.image_url, p.average_rating,
-            c.name AS category_name
-         FROM products p
-         INNER JOIN categories c ON p.category_id = c.id
-         WHERE p.is_active = 1
-           AND (c.id = ? OR c.parent_id = ?)
-         ORDER BY p.id DESC`,
-        [categoryId, categoryId]
-    );
-
-    return rows;
-};
-module.exports = {
-    allProducts
-
-
-
-
-};
+const db = require('../config/db');
+const allProducts = async (categoryId = null) => {
+    if (!categoryId) {
+    const [rows] = await db.execute(
+            `SELECT
+                p.id, p.name, p.slug, p.description, p.base_price, p.sale_price,
+                p.stock_quantity, p.image_url, p.average_rating,
+                c.name AS category_name
+              FROM products p
+             INNER JOIN categories c ON p.category_id = c.id
+             WHERE p.is_active = 1
+             ORDER  BY p.id DESC`
+        );
+          return rows;
+    }
+    const [rows] = await db.execute(
+        `SELECT
+            p.id, p.name, p.slug, p.description, p.base_price, p.sale_price,
+            p.stock_quantity, p.image_url, p.average_rating,
+            c.name AS category_name
+         FROM products p
+         INNER JOIN categories c ON p.category_id = c.id
+         WHERE p.is_active = 1
+           AND (c.id = ? OR c.parent_id = ?)
+         ORDER BY p.id DESC`,
+        [categoryId, categoryId]
+    );
+
+    return rows;
+};
+module.exports = {
+    allProducts
+
+
+
+
+};
