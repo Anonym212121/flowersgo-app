19a71f3efc50a53168dde045654d67990408f515
Сковлюк Матвій Сергійович <ipz224_sms@student.ztu.edu.ua>
2026-04-03 02:51:00 +0300

    made wishlist routes

--- backend/src/routes/pagesRoutes.js
+++ backend/src/routes/pagesRoutes.js
@@ -3,6 +3,7 @@ const express = require('express');
 const pagesController = require('../controllers/pagesController');
 const pageAuthContext = require('../middleware/pageAuthContext');
 const pageRequireLogin = require('../middleware/pageRequireLogin');
+const wishlistController = require('../controllers/wishlistController');
 
 const router = express.Router();
 router.use(pageAuthContext);
@@ -12,5 +13,8 @@ router.get('/login', pagesController.loginPage);
 router.get('/register', pagesController.registerPage);
 router.get('/logout', pagesController.logout);
 router.get('/cabinet', pageRequireLogin, pagesController.cabinetPage);
+router.get('/wishlist', pageRequireLogin, wishlistController.wishlistPage);
+router.post('/wishlist/add', pageRequireLogin, wishlistController.addProduct);
+router.post('/wishlist/delete', pageRequireLogin, wishlistController.deletewishProduct);
 module.exports = router;
 
