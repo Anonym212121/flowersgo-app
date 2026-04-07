e159b0d6394a4dc6061b68d04f8a92c7fa197469
Сковлюк Матвій Сергійович <ipz224_sms@student.ztu.edu.ua>
2026-04-08 01:56:42 +0300

    created adminApiRouter

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,19 +8,20 @@ const rootDir = path.join(__dirname, '..');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(rootDir, 'views'));
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 const pagesRoutes = require('./routes/pagesRoutes');
 app.use('/', pagesRoutes);
 app.use(express.static(path.join(rootDir, 'public')));
 
 
-
-
 const authRoutes = require('./routes/authRoutes');
 app.use('/api/auth', authRoutes);
+
+const pageAuthContext = require('./middleware/pageAuthContext');
+const requireAdminJson = require('./middleware/requireAdminJson');
+const adminApiRoutes = require('./routes/adminApiRoutes');
+app.use('/api/admin', pageAuthContext, requireAdminJson, adminApiRoutes);
 
 const seedDefaults = require('./services/seedDefaults');
 
@@ -36,5 +37,4 @@ const start = async () => {
         console.log(`: ${PORT}`);
     });
 };
-
 start();
