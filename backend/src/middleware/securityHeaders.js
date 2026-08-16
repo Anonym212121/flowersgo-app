a91c7ecd5661c1d7fee60413ae402dd7581d1b81
Сковлюк Матвій Сергійович <ipz224_sms@student.ztu.edu.ua>
2026-08-16 11:39:04 +0300

    fix problems- googleauth

--- backend/src/middleware/securityHeaders.js
+++ backend/src/middleware/securityHeaders.js
@@ -3,6 +3,7 @@ const helmet = require('helmet');
 const helmetMw = helmet({
     contentSecurityPolicy: false,
     crossOriginEmbedderPolicy: false,
+    crossOriginOpenerPolicy: { policy: 'same-origin-allow-popups' },
     hsts: false,
     referrerPolicy: { policy: 'strict-origin-when-cross-origin' }
 });
